804687732bf82d46fdb6b2200bcc4f9264c2be1f
gunya999-cmd <gunya999@gmail.com>
2026-07-17 12:53:17 +0300

    Load modern design refresh after legacy styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { LessonPageNavigator } from './LessonPageNavigator';
 import './styles.css';
 import './adaptive.css';
 import './map.css';
+import './designRefresh.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
