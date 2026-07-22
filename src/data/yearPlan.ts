0c7d4d7f61af7270610f48c632027bfaf61be765
gunya999-cmd <gunya999@gmail.com>
2026-07-22 20:51:03 +0300

    Mark lesson 5 available in the official year plan

--- src/data/yearPlan.ts
+++ src/data/yearPlan.ts
@@ -97,7 +97,7 @@ export const yearPlan: YearLesson[] = segments.flatMap(segment => {
     paragraph:segment.paragraph,
     title:segment.topic,
     lessonType:lessonType(segment, offset),
-    available:segment.from + offset <= 4,
+    available:segment.from + offset <= 5,
     topicLessonIndex:offset + 1,
     topicLessonCount,
   }));
