560142cbe8e87753979c86d9987863d581d03134
gunya999-cmd <gunya999@gmail.com>
2026-07-26 14:09:26 +0300

    Open lesson 7 in official year plan

--- src/data/yearPlan.ts
+++ src/data/yearPlan.ts
@@ -97,7 +97,7 @@ export const yearPlan: YearLesson[] = segments.flatMap(segment => {
     paragraph:segment.paragraph,
     title:segment.topic,
     lessonType:lessonType(segment, offset),
-    available:segment.from + offset <= 6,
+    available:segment.from + offset <= 7,
     topicLessonIndex:offset + 1,
     topicLessonCount,
   }));
