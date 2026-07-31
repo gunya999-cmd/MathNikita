5f454d3011916093335b0784712ac8d383fe7687
gunya999-cmd <gunya999@gmail.com>
2026-07-31 13:48:47 +0300

    Register lesson 14 extended practice

--- src/data/extendedPracticeData.ts
+++ src/data/extendedPracticeData.ts
@@ -14,6 +14,7 @@ import { extendedPracticeLesson10 } from './extendedPracticeLesson10';
 import { extendedPracticeLesson11 } from './extendedPracticeLesson11';
 import { extendedPracticeLesson12 } from './extendedPracticeLesson12';
 import { extendedPracticeLesson13 } from './extendedPracticeLesson13';
+import { extendedPracticeLesson14 } from './extendedPracticeLesson14';
 
 const lesson3: ExtendedPracticeSet = {
   title:'Тренировочная мастерская: десятичная запись',
@@ -43,6 +44,7 @@ export const extendedPracticeByLesson: Record<number, ExtendedPracticeSet> = {
   11:extendedPracticeLesson11,
   12:extendedPracticeLesson12,
   13:extendedPracticeLesson13,
+  14:extendedPracticeLesson14,
 };
 
 export const extendedPracticeLessonNumbers = Object.keys(extendedPracticeByLesson).map(Number);
