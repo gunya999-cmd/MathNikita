b9ce53d68698f529b9960159a27c5a442f24e258
gunya999-cmd <gunya999@gmail.com>
2026-08-30 19:13:49 +0300

    Register lesson 86 mandatory practice

--- src/data/extendedPracticeData.ts
+++ src/data/extendedPracticeData.ts
@@ -2,8 +2,9 @@ import {extendedPracticeByLesson as baseExtendedPracticeByLesson} from './extend
 import {extendedPracticeLesson83} from './extendedPracticeLesson83';
 import {extendedPracticeLesson84} from './extendedPracticeLesson84';
 import {extendedPracticeLesson85} from './extendedPracticeLesson85';
+import {extendedPracticeLesson86} from './extendedPracticeLesson86';
 import type {ExtendedPracticeSet} from './extendedPracticeTypes';
 
-export const extendedPracticeByLesson:Record<number,ExtendedPracticeSet>={...baseExtendedPracticeByLesson,83:extendedPracticeLesson83,84:extendedPracticeLesson84,85:extendedPracticeLesson85};
+export const extendedPracticeByLesson:Record<number,ExtendedPracticeSet>={...baseExtendedPracticeByLesson,83:extendedPracticeLesson83,84:extendedPracticeLesson84,85:extendedPracticeLesson85,86:extendedPracticeLesson86};
 export const extendedPracticeLessonNumbers=Object.keys(extendedPracticeByLesson).map(Number);
 export type{ExtendedPracticeSet,ExtendedPracticeTask}from'./extendedPracticeTypes';
