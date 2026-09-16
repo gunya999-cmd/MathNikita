af2b148748929513de3df1777dc18d1cc4a445d7
gunya999-cmd <gunya999@gmail.com>
2026-09-16 09:16:21 +0300

    Register lesson 148 extended practice

--- src/data/extendedPracticeData.ts
+++ src/data/extendedPracticeData.ts
@@ -12,8 +12,9 @@ import {extendedPracticeLesson144} from './extendedPracticeLesson144';
 import {extendedPracticeLesson145} from './extendedPracticeLesson145';
 import {extendedPracticeLesson146} from './extendedPracticeLesson146';
 import {extendedPracticeLesson147} from './extendedPracticeLesson147';
+import {extendedPracticeLesson148} from './extendedPracticeLesson148';
 import type {ExtendedPracticeSet} from './extendedPracticeTypes';
 
-export const extendedPracticeByLesson:Record<number,ExtendedPracticeSet>={...baseExtendedPracticeByLesson,83:extendedPracticeLesson83,84:extendedPracticeLesson84,85:extendedPracticeLesson85,86:extendedPracticeLesson86,87:extendedPracticeLesson87,88:extendedPracticeLesson88,89:extendedPracticeLesson89,141:extendedPracticeLesson141,143:extendedPracticeLesson143,144:extendedPracticeLesson144,145:extendedPracticeLesson145,146:extendedPracticeLesson146,147:extendedPracticeLesson147};
+export const extendedPracticeByLesson:Record<number,ExtendedPracticeSet>={...baseExtendedPracticeByLesson,83:extendedPracticeLesson83,84:extendedPracticeLesson84,85:extendedPracticeLesson85,86:extendedPracticeLesson86,87:extendedPracticeLesson87,88:extendedPracticeLesson88,89:extendedPracticeLesson89,141:extendedPracticeLesson141,143:extendedPracticeLesson143,144:extendedPracticeLesson144,145:extendedPracticeLesson145,146:extendedPracticeLesson146,147:extendedPracticeLesson147,148:extendedPracticeLesson148};
 export const extendedPracticeLessonNumbers=Object.keys(extendedPracticeByLesson).map(Number);
 export type{ExtendedPracticeSet,ExtendedPracticeTask}from'./extendedPracticeTypes';
